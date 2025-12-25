9d42ad3198d5372874ec77c10fd38b6d875a3062
juniorpen01 <01juniorpen@gmail.com>
2025-12-26 02:59:51 +0800

    Learn to implement classes, use arrays and vectors, implement constructors, overlaod operators, etc etc

--- LearnCpp/LearnCpp.cpp
+++ LearnCpp/LearnCpp.cpp
@@ -1,7 +1,35 @@
 #include <memory>
+#include <vector>
+#include <array>
 
 #pragma warning(push)
 #pragma warning(disable:4189) // unused variables
+
+// put classes above main to not deal with forward decl typa shi
+class Widget { // apparently just pascalcase class names
+private: // apparently private before public
+	std::unique_ptr<std::array<int, 1'000'000>> data; // std array has size and safer; what cpp uses 's instead of _s
+public:
+	Widget() : data(std::make_unique<std::array<int, 1'000'000>>()) {} // btw for some reason u still have to explicitly type the generic for make unique
+
+	// ok so i have to explicitly delete some constructors
+	Widget(const Widget&) = delete;
+	Widget& operator=(const Widget&) = delete;
+}; // ok so im an idiot and just found out that im supposed to use a size arg to determine size of the data, so therefore i should be using vector, but i already wrote a lot so why not
+
+// NOTE: revelation 2: memory leak just basically when a process takes up memory (perhaps more accurately so-called "resources"?) up until it exits, leaving it unavaiable to other processes; perhaps to add it's when a "resource" truly not needed anymore is not released
+
+class Widget2 { // let's implement with vector this time
+private:
+	std::vector<int> data;
+public:
+	// let's do size_t instead
+	Widget2(size_t size) // btw i dont wanna do const int in func params for to the eyes of the end user it truly doesnt matter; i believe it matters for references tho using my intuition
+	{
+		data.reserve(size); // reserve actually does preallocation
+	} // data(size) means allocating all zeroed out of that size, not preallocation
+};
+
 
 int main() {
 	// resources and smart ptrs
@@ -16,6 +44,13 @@ int main() {
 	std::unique_ptr<int> c(new int(4)); // takes a ptr; WARN: dont do this cuz less ergonomic; also msvc has weird ahh error messages, it talks about a deleted copy constructor, like what?? it's deleted bruh but apparently has smth to do with overload resolution
 
 	// NOTE: ok let's forget the semantics of other languages and accept the way u do in cpp cuz too mental
+
+	// btw there shared and weak ptrs
+
+	// ok this time let's give more sensical names, otherwise if so primitive
+
+	Widget widget;
+	Widget2 widget2(1'000'000);
 }
 
 #pragma warning(pop)
